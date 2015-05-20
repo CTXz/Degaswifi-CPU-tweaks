9c37287232a7afe3af33ef987be606a47e07397a
ctxgaming <patrick_pedersen_02032001@yahoo.com>
2015-05-20 17:13:41 +0200

    ported adaptive cpu gov from friedrich420

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -401,6 +401,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_dancedance)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
+extern struct cpufreq_governor cpufreq_gov_adaptive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_adaptive)
 #endif
 
 
