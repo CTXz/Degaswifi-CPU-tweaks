da1cbcd4edbf775d3e966dc2602d195b8e98585a
ctxgaming <patrick_pedersen_02032001@yahoo.com>
2015-05-20 21:58:51 +0200

    ported lionheart from friedrich420 + added earlysuspend.h to fix compiling error

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -404,6 +404,9 @@ extern struct cpufreq_governor cpufreq_gov_dancedance;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
 extern struct cpufreq_governor cpufreq_gov_adaptive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_adaptive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionheart)
 #endif
 
 
